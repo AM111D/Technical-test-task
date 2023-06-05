8fe29f98d0cf3271c7992acaf266566567726f9b
AM111D <Dimasssnike@icloud.com>
2023-06-05 03:37:47 -0400

    Update HomeView.js

--- src/components/views/HomeView.js
+++ src/components/views/HomeView.js
@@ -5,7 +5,7 @@ const HomeView = () => (
   <div className={css.container}>
     <h1 className={css.title}>
       Hi 🤩 I'm glad to welcome you in the new application,
-      <br /> go to the page with cards to subscribe to popular users{' '}
+      <br /> go to the page with tweets to subscribe to popular users{' '}
       <span role="img" aria-label="Иконка приветствия" className={css.emoji}>
         😎
       </span>
